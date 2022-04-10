bc3a863fa797f8a7983fe46d51719780dacb5afc
Евгений <egoirk@gmail.com>
2022-04-11 04:47:49 +0800

    Fix r

--- source/js/main.js
+++ source/js/main.js
@@ -8,57 +8,12 @@ navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
-    navLogo.classList.remove('main-nav--closed');
-    navLogo.classList.add('main-nav--opened');
+    navLogo.classList.remove('page-header__logo--closed');
+    navLogo.classList.add('page-header__logo--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
-    navLogo.classList.add('main-nav--closed');
-    navLogo.classList.remove('main-nav--opened');
+    navLogo.classList.add('page-header__logo--closed');
+    navLogo.classList.remove('page-header__logo--opened');
   }
 });
-
-
-// ---------------------------------
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
-  iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
-});
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
