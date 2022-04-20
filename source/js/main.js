034bb8719c33cdee66ed67fccce60b484c818f2a
Евгений <egoirk@gmail.com>
2022-04-21 05:11:12 +0800

    fix pixel perfect and js

--- source/js/main.js
+++ source/js/main.js
@@ -2,23 +2,36 @@ const bodyOverflow = document.querySelector('.page-body');
 const navLogo = document.querySelector('.page-header__logo');
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const clickMenu = document.querySelector('.main-nav__list');
 
 navMain.classList.remove('main-nav--nojs');
 
+function openMenu() {
+  bodyOverflow.classList.remove('page-body__visible');
+  bodyOverflow.classList.add('page-body__hidden');
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  navLogo.classList.remove('page-header__logo--closed');
+  navLogo.classList.add('page-header__logo--opened');
+}
+
+function closeMenu() {
+  bodyOverflow.classList.remove('page-body__hidden');
+  bodyOverflow.classList.add('page-body_visible');
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+  navLogo.classList.add('page-header__logo--closed');
+  navLogo.classList.remove('page-header__logo--opened');
+}
+
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
-    bodyOverflow.classList.remove('page-body__visible');
-    bodyOverflow.classList.add('page-body__hidden');
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navLogo.classList.remove('page-header__logo--closed');
-    navLogo.classList.add('page-header__logo--opened');
+    openMenu();
   } else {
-    bodyOverflow.classList.remove('page-body__hidden');
-    bodyOverflow.classList.add('page-body_visible');
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navLogo.classList.add('page-header__logo--closed');
-    navLogo.classList.remove('page-header__logo--opened');
+    closeMenu();
   }
+});
+
+clickMenu.addEventListener('click', function () {
+  closeMenu();
 });
